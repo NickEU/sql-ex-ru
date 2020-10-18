219d3299bb8a8ca65bfec6b32db7248dd9f9930b
Nikolajs Korolkovs <k@nick.tf>
2020-10-18 06:38:58 +0300

    noob-tier: upd 5

--- noob-tier/05.sql
+++ noob-tier/05.sql
@@ -2,4 +2,4 @@
 -- of PCs cheaper than $600 having a 12x or a 24x CD drive.
 
 SELECT model, speed, hd FROM PC
-WHERE price < 600 AND (cd = '12x' OR cd = '24x')
+WHERE price < 600 AND cd IN ('12x', '24x')
