4843b4f8c1b04131dd0d50fb1416be8e1dc42085
NickEU <neverate@gmail.com>
2020-10-21 15:03:18 +0300

    noob-tier: upd 18

--- noob-tier/18.sql
+++ noob-tier/18.sql
@@ -3,3 +3,10 @@
 SELECT DISTINCT maker, price FROM Product, Printer
 WHERE Product.model = Printer.model AND color = 'y'
 AND price = (SELECT MIN(price) FROM Printer WHERE color = 'y')
+
+-- using join
+
+SELECT maker, price FROM Product
+JOIN Printer ON Product.model = Printer.model AND color = 'y'
+WHERE price = (SElECT MIN(price) FROM Printer WHERE color = 'y')
+GROUP BY maker, price
